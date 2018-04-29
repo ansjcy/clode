9197e56029b82b8105405c1f3d10d80f50ac36e7
Haolin <3130103807@zju.edu.cn>
2018-04-29 19:25:21 -0400

    blockchain

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -66,8 +66,102 @@ class Blockchain:
             self.isps[isp_name] = (self.isp_num, isp_address)
             self.isp_num += 1
 
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: A blockchain
+        :return: True if valid, False if not
+        """
 
+        previous_block = chain[0]
+        current_index = 1
 
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{previous_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            previous_block_hash = self.hash(previous_block)
+            if block['previous_hash'] != previous_block_hash:
+                return False
+
+            if not self.valid_proof(previous_block['proof'], block['proof'], previous_block_hash):
+                return False
+
+            previous_block = block
+            current_index += 1
+
+        return True
+
+    def __getitem__(self, idx):
+        return self.chain[idx]
+
+    def resolve_conflicts(self):
+        """
+        consensus algorithm
+        choose one with longest block
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(neighbours[node][1] + "/get_chain")
+
+            if response.status_code == 200:
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if len(chain) > max_length and self.valid_chain(chain):
+                    max_length = len(chain)
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    def proof_of_work(self, last_block):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes
+         - Where p is the previous proof, and p' is the new proof
+
+        :param last_block: <dict> last Block
+        :return: <int>
+        """
+
+        last_proof = last_block['proof']
+        last_hash = self.hash(last_block)
+
+        proof = 0
+        while self.valid_proof(last_proof, proof, last_hash) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof, last_hash):
+        """
+        Validates the Proof
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :param last_hash: <str> The hash of the Previous Block
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
 # Instantiate the Node
 app = Flask(__name__)
@@ -81,8 +175,6 @@ blockchain = Blockchain()
 buffer = {
     # transaction_id : [{}]
 }
-
-
 
 @app.route('/crypto', methods=['POST'])
 def post_crypto():
@@ -105,12 +197,6 @@ def post_crypto():
 
         # ready to do evaluation.
 
-
-
-
-
-
-
 @app.route('/register_node', methods=['POST'])
 def post_crypto():
     values = request.get_json()
@@ -131,8 +217,6 @@ def post_crypto():
 @app.route('/register_isp', methods=['POST'])
 def register_isp():
     return
-
-
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
