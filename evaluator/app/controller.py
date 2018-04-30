a9adda1ba981dfd352859bd9f51d42ab998d45a7
Huijuan <huijuan.huang@yale.edu>
2018-04-29 23:25:57 -0400

    finish node server

--- evaluator/app/controller.py
+++ evaluator/app/controller.py
@@ -1,5 +1,19 @@
 from app import app
+from app import node
+from flask import request
+import json
+import config
 
-@app.route('/query', method='POST')
+@app.route('/query', methods=['POST'])
 def query():
-    return 'Hello world ~~~~~~'
+    values = request.get_json()
+    required = ['crypto_list']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    crypto_list = values['crypto_list']
+    remain_shuf_times = config.NUM_NODES
+    if values['remain_shuf_times'] != None:
+        remain_shuf_times = values['remain_shuf_times']
+    overlap = node.get_overlap(crypto_list, remain_shuf_times)
+    query_result = {'overlap': overlap}
+    return json.dumps(query_result)
