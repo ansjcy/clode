2fc4ac5d378ef533f74bec5af0b55a50a7ee6cfa
Jason Chenyang Ji <chenyangjizju@gmail.com>
2018-04-30 15:38:38 -0400

    fix a bug

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -328,7 +328,7 @@ if __name__ == '__main__':
     print(myname)
     print(myaddr)
     if myaddr != config.blockchain_address:
-        res = requests.post(url='http://'+config.blockchain_address+'/register_node', data={'address': myaddr})
+        res = requests.post(url='http://'+config.blockchain_address+'/register_node', data={'address': myaddr}).json()
         blockchain.nodes = set(res['address_list'])
 
     from argparse import ArgumentParser
